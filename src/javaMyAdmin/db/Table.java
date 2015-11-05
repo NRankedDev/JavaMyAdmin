8703446bbe9f87a27ef132cba569646161f58caa
Normynator <Norman.Ziebal@web.de>
2015-11-05 20:00:43 +0100

    Rework Table and Rework Issue #21

--- src/javaMyAdmin/db/Table.java
+++ src/javaMyAdmin/db/Table.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 
-public class Table{
+public class Table {
 	private ArrayList<String> columnNames = new ArrayList<String>();;
 	private ArrayList<Line> lines = new ArrayList<Line>();
 	private String name;
@@ -39,6 +39,55 @@ public class Table{
 		lines.clear();
 	}
 
+	public ArrayList<String> getColumnNames() throws SQLException {
+		return columnNames;
+	}
+
+	public String getColumnNames(int i) throws SQLException {
+		return columnNames.get(i);
+	}
+
+	public ArrayList<Line> getLines() throws SQLException {
+		loadLines(null);
+		return lines;
+	}
+
+	public Line getLines(int i) throws SQLException {
+		if (lines.isEmpty()) {
+			loadLines(null);
+		}
+		return lines.get(i);
+	}
+	
+	public String getDatentyp(String column) throws SQLException{
+		return getColumnInfo(column, 1);
+	}
+	
+	public String getLength(String column) throws SQLException{
+		return getColumnInfo(column, 2);
+	}
+	
+	public boolean getNull(String column) throws SQLException{
+		return getColumnInfo(column, 3).equals(true) ? true : false;
+	}
+	
+	public String getIndex(String column) throws SQLException{
+		return getColumnInfo(column, 4);
+		
+	}
+
+	public ArrayList<Line> getLines(ResultSet rs) throws SQLException {
+		loadLines(rs);
+		return lines;
+	}
+	
+	public void setValue(int line, int column, String value) throws SQLException {
+		if (lines.isEmpty()) {
+			loadLines(null);
+		}
+		connect.createStatement().executeUpdate("UPDATE `"+dbname+"`.`" + getName() + "` SET `" + getColumnNames(column) + "` = '" + value + "' WHERE `" + getColumnNames(0) + "` = " + getLines(line).getValues(0));
+	}
+
 	public void loadLines(ResultSet rs) throws SQLException {
 		clear();
 		if (rs == null) {
@@ -62,38 +111,6 @@ public class Table{
 			}
 			AddLine(line);
 		}
-	}
-
-	public ArrayList<String> getColumnNames() throws SQLException {
-		return columnNames;
-	}
-
-	public String getColumnNames(int i) throws SQLException {
-		return columnNames.get(i);
-	}
-
-	public ArrayList<Line> getLines() throws SQLException {
-		loadLines(null);
-		return lines;
-	}
-
-	public Line getLines(int i) throws SQLException {
-		if (lines.isEmpty()) {
-			loadLines(null);
-		}
-		return lines.get(i);
-	}
-
-	public ArrayList<Line> getLines(ResultSet rs) throws SQLException {
-		loadLines(rs);
-		return lines;
-	}
-	
-	/* test */public void setValue(int line, int column, String value) throws SQLException {
-		if (lines.isEmpty()) {
-			loadLines(null);
-		}
-		connect.createStatement().executeUpdate("UPDATE `"+dbname+"`.`" + getName() + "` SET `" + getColumnNames(column) + "` = '" + value + "' WHERE `" + getColumnNames(0) + "` = " + getLines(line).getValues(0));
 	}
 	
 	public void addTupel(ArrayList<String> input) throws SQLException{
@@ -121,20 +138,34 @@ public class Table{
 		}
 		connect.createStatement().executeUpdate(cmd);
 	}
+	
 	public ArrayList<Line> search(String suche, int column) throws SQLException {
 		return getLines(connect.createStatement().executeQuery("SELECT * FROM `" + getName() + "` WHERE `" + getColumnNames(column) + "` =" + suche));
 	}
-	public String getDatentyp(String column) throws SQLException{
-		return connect.createStatement().executeQuery("SELECT * FROM `" + getName() + "`").getMetaData().getColumnTypeName(columnNames.indexOf(column)+1);
-	}
-	public String getLength(String column) throws SQLException{
-		return Integer.toString(connect.createStatement().executeQuery("SELECT * FROM `" + getName() + "`").getMetaData().getColumnDisplaySize(columnNames.indexOf(column)+1));
-	}
-	public boolean getNull(String column) throws SQLException{
-		return  connect.createStatement().executeQuery("SELECT * FROM `" + getName() + "`").getMetaData().isNullable(columnNames.indexOf(column)+1) != 0;
-	}
-	public String getIndex(String column) throws SQLException{
+	
+	public String getColumnInfo(String column, int i) throws SQLException{
+		String value;
 		ResultSet rs;
-		return (rs = connect.createStatement().executeQuery("select `COLUMN_KEY` from information_schema.columns where table_name='"+getName() +"' and column_name like '"+column+"'")).next() ? rs.getString(1) : null;
+		
+		switch(i){
+		case 1:
+			value = "DATA_TYP";
+			break;
+		case 2:
+			value = "CHARAKTER_MAXIMUM_LENGTH";
+			break;
+		case 3:
+			value = "IS_NULLABLE";
+			break;
+		case 4:
+			value = "COLUMN_KEY";
+			break;
+		default:
+			value = null;
+			break;
+		}
+	
+		return (rs=connect.createStatement().executeQuery("select `"+value+"` from information_schema.columns where table_name='"+name+"' and column_name like '"+column+"'")).next() ? rs.getString(1) : null;
 	}
+
 }
